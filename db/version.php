5876af79fd669a8b6fdaa8c3675b3565dd398a2c
dewray <35580043+dewray@users.noreply.github.com>
2022-12-18 11:45:34 +0900

    Create version.php

---
+++ db/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'mod_completiongrade';
+$plugin->version = 2022080100;
+$plugin->release = 'v1.0';
+$plugin->requires = 2018051700;
+$plugin->maturity = MATURITY_ALPHA;
